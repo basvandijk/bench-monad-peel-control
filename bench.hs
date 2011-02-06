fd3165887f4614d8eeda3a51330bfe76645345e7
Bas van Dijk <v.dijk.bas@gmail.com>
2011-02-06 13:41:42 +0100

    Added some more benchmarks
    Ignore-this: 992c9724cf8be83ad966762e5d24712a
    
    darcs-hash:20110206124142-ae560-4e6e1b416cda3ade55279259551416a2a225f2f1.gz

--- bench.hs
+++ bench.hs
@@ -1,33 +1,101 @@
+{-# LANGUAGE DeriveDataTypeable, RankNTypes #-}
+
 module Main where
+
+
+--------------------------------------------------------------------------------
+-- Imports
+--------------------------------------------------------------------------------
+
+-- from base:
+import Prelude hiding (catch)
+import Control.Exception ( Exception, SomeException, throwIO )
+import qualified Control.Exception as E ( mask )
+import Data.Typeable
+import Control.Monad (join)
 
 -- from criterion:
 import Criterion.Main
 
--- from mtl:
-import Control.Monad.Reader
-import Control.Monad.State
-import Control.Monad.Writer
+-- from transformers:
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State
+import Control.Monad.Trans.Writer
 
 -- from monad-peel:
-import qualified Control.Exception.Peel as Peel
+import qualified Control.Exception.Peel as MP
+import qualified Control.Monad.IO.Peel  as MP
 
 -- from monad-control:
-import qualified Control.Exception.Control as MonadControl
+import qualified Control.Exception.Control as MC
+
+
+--------------------------------------------------------------------------------
+-- Main
+--------------------------------------------------------------------------------
 
 main :: IO ()
 main = defaultMain
-         [ bench "Peel.bracket"         $ b Peel.bracket
-         , bench "MonadControl.bracket" $ b MonadControl.bracket
+         [ benchBoth "bracket"  benchBracket  MP.bracket  MC.bracket
+         , benchBoth "bracket_" benchBracket_ MP.bracket_ MC.bracket_
+         , benchBoth "catch"    benchCatch    MP.catch    MC.catch
+         , benchBoth "try"      benchTry      MP.try      MC.try
+         , benchBoth "mask"     benchMask     mpMask      MC.mask
          ]
 
-type M a = ReaderT Int (StateT Bool (WriterT String IO)) a
+benchBoth name bnch peel mndCtrl = bgroup name
+  [ bench "monad-peel"    $ bnch peel
+  , bench "monad-control" $ bnch mndCtrl
+  ]
 
-runM :: Int -> Bool -> M a -> IO ((a, Bool), String)
-runM r s m = runWriterT (runStateT (runReaderT m r) s)
 
-b bracket =
-  runM 0 False $
-    bracket
-      (ask >>= \r -> modify (\s -> s || even r) >> tell "Hello" >> liftIO (return 1))
-      (\n -> liftIO (return n) >>= \x -> tell " " >> get >>= \s -> tell "World" >> ask >>= \r -> put (odd r && s))
-      (\n -> get >>= \s -> ask >>= \r -> tell (" state=" ++ (show s)) >> tell (" env=" ++ (show r)) >> liftIO (return n))
+--------------------------------------------------------------------------------
+-- Monad stack
+--------------------------------------------------------------------------------
+
+type M a = ReaderT Int (StateT Bool (WriterT String (MaybeT IO))) a
+
+type R a = IO (Maybe ((a, Bool), String))
+
+runM :: Int -> Bool -> M a -> R a
+runM r s m = runMaybeT (runWriterT (runStateT (runReaderT m r) s))
+
+exe :: M a -> R a
+exe = runM 0 False
+
+
+--------------------------------------------------------------------------------
+-- Benchmarks
+--------------------------------------------------------------------------------
+
+benchBracket  bracket  = exe $ bracket nop (\_ -> nop) (\_ -> nop)
+benchBracket_ bracket_ = exe $ bracket_ nop nop nop
+benchCatch    catch    = exe $ catch throwE (\E -> nop)
+benchTry      try      = exe $ try throwE :: R (Either E ())
+
+benchMask :: (((forall a. M a -> M a) -> M ()) -> M ()) -> R ()
+benchMask mask = exe $ mask $ \restore -> nop >> restore nop >> nop
+
+
+--------------------------------------------------------------------------------
+-- Utils
+--------------------------------------------------------------------------------
+
+nop :: Monad m => m ()
+nop = return ()
+
+data E = E deriving (Show, Typeable)
+
+instance Exception E
+
+throwE = liftIO $ throwIO E
+
+mpMask :: MP.MonadPeelIO m => ((forall a. m a -> m a) -> m b) -> m b
+mpMask f = do
+  k <- MP.peelIO
+  join $ liftIO $ E.mask $ \restore -> k $ f $ MP.liftIOOp_ restore
+
+
+-- The End ---------------------------------------------------------------------
